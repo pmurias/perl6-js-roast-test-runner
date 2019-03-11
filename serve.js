db16b3d100858a14fdbf66bd9e6984864dc0299d
Paweł Murias <pawelmurias@gmail.com>
2019-03-11 16:13:09 +0100

    Run test in proper function

--- serve.js
+++ serve.js
@@ -10,24 +10,57 @@ app.get('/', (req, res) => {
 app.use(express.static('static'));
 app.use(express.static('tests'));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const server = app.listen(port, () => true);
 
 const puppeteer = require('puppeteer');
 
 (async () => {
-  try {
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
-    page.on('console', msg => {
-      if (msg.type() === 'log') {
-        console.log('tap:', msg.text());
-      } else {
-        console.log('got msg', msg);
-      }
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+
+  function runTest() {
+    return new Promise(function(resolve, reject) {
+      (async () => {
+        try {
+          page.on('error', error => {
+            reject(error);
+          });
+
+          page.on('pageerror', error => {
+            reject(error);
+          });
+
+          page.on('console', msg => {
+            if (msg.type() === 'log') {
+              console.log('tap:', msg.text());
+            } else if (msg.type() === 'info' && msg.text() === 'end') {
+              console.log('test ended');
+              resolve();
+            } else {
+              console.log('got msg', msg);
+             // resolve();
+            }
+          });
+
+          await page.goto('http://localhost:3000/load_test.html');
+        } catch (e) {
+          console.log('error', e);
+        }
+        
+
+      })();
     });
-    await page.goto('http://localhost:3000/load_test.html');
-//    await browser.close();
-  } catch (e) {
-    console.log('error', e);
   }
+
+  console.log('running test');
+  try {
+    await runTest();
+  } catch (e) {
+    console.log(`can't run test`, e);
+  }
+  console.log('test completed');
+
+  server.close();
+
+  await browser.close();
 })();
