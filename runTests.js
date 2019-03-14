fdc1937143553fe6971d864569d943e9160c346f
Paweł Murias <pawelmurias@gmail.com>
2019-03-14 10:03:42 +0100

    Support for running a single test

--- runTests.js
+++ runTests.js
@@ -14,7 +14,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({headless: true});
 
-  function runTest(file, tap) {
+  function runTest(file, gotTap) {
     console.log('running', file);
 
     return new Promise(function(resolve, reject) {
@@ -23,6 +23,7 @@ const puppeteer = require('puppeteer');
         const page = await browser.newPage();
 
         async function gotError(error) {
+          console.log('got error', error);
           await page.close();
           reject(error);
         }
@@ -34,17 +35,17 @@ const puppeteer = require('puppeteer');
           page.on('console', msg => {
             if (msg.type() === 'log') {
               //console.log('tap:', msg.text());
-              tap.push(msg.text());
+              gotTap(msg.text());
             } else if (msg.type() === 'info' && msg.text() === 'end') {
               (async () => {
                 await page.close();
                 resolve();
               })();
             } else if (msg.type() === 'info' && msg.text() === 'exit ') {
-              tap.push('EXIT');
+              gotTap('EXIT');
             } else if (msg.type() === 'error') {
               console.log('error:', msg.text());
-              tap.push(msg.text());
+              gotTap(msg.text());
             } else {
               console.log('got msg', msg);
             }
@@ -68,23 +69,39 @@ const puppeteer = require('puppeteer');
   async function runTestAndWriteTap(file) {
     const tap = [];
     try {
-      await runTest(file, tap);
+      await runTest(file, tapPart => tap.push(tapPart));
     } catch (e) {
       tap.push(e.toString());
     }
     fs.writeFileSync('output/' + file.replace('\.js', '.tap'), tap.join('\n'));
   }
 
-  try {
-    const tap = [];
+  console.log(process.argv);
 
-    for (const test of fs.readdirSync('tests').filter(test => /.js$/.test(test))) {
-      await runTestAndWriteTap(test);
+  if (process.argv.length === 2) {
+    try {
+      for (const test of fs.readdirSync('tests').filter(test => /.js$/.test(test))) {
+        await runTestAndWriteTap(test);
+      }
+    } catch (e) {
+      console.log(`can't run test`, e);
     }
-  } catch (e) {
-    console.log(`can't run test`, e);
+  } else {
+      for (const test of process.argv.slice(2)) {
+        const tap = [];
+
+        try {
+          await runTest(test, tapPart => {
+            tap.push(tapPart);
+            console.log(tapPart)
+          });
+        } catch (e) {
+          console.log(e);
+        }
+
+        fs.writeFileSync('output/' + test.replace('\.js', '.tap'), tap.join('\n'));
+      }
   }
-  console.log('test completed');
 
   server.close();
 
